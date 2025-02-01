d80d9281b701db1362549c56300b0c76a64a63d3
LyMinh85 <mlym8565@gmail.com>
2025-02-01 10:51:46 +0700

    Fixed bugs.

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,26 +1,12 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export async function POST(request: Request) {
-  // try {
-  //   const res = await request.json();
-  //   const user = await prisma.user.create({
-  //     data: {
-  //       email: res.email,
-  //       password: res.password,
-  //     },
-  //   });
-
-  //   if (user === null) {
-  //     console.error('User not created');
-  //   }
-
-  //   return Response.json(user, { status: 201 });
-  // } catch (error) {
-  //   if (error instanceof Prisma.PrismaClientKnownRequestError) {
-  //     console.error(error.message);
-  //   }
-  //   return Response.json({ error: 'Internal Server Error' }, { status: 500 });
-  // }
+export async function POST() {
+  return new Response(
+    JSON.stringify({
+      message: "This is a POST request",
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 }
